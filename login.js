21c8a7014cd153032365c8ea44f164ba3a8b9f27
bish <bishsefi@gmail.com>
2021-04-10 13:00:53 -0400

    Adding page when logged in

--- login.js
+++ login.js
@@ -17,27 +17,36 @@ app.use(session({
 	resave: true,
 	saveUninitialized: true
 }));
+/*app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');*/
+
+
+
+
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-app.use('/css',express.static('css'));
+
+
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname,'/index.html'));
+	response.sendFile(path.join(__dirname,'/public/index.html'));
 });
 
+app.use('/css',express.static(__dirname + '/public/css'));
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
 
 
 app.post('/auth', function(request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
-	if (username && password) {
+	if (username && password) {``
 		connection.query('SELECT * FROM login_table WHERE uname = ? AND psswd = ?', [username, password], function(error, results, fields) {
 			if (results.length > 0) {
 				request.session.loggedin = true;
 				request.session.username = username;
-				console.log("success");
 				response.redirect('/test');
 			} else {
 				response.send('Incorrect Username and/or Password!');
@@ -53,7 +62,9 @@ app.post('/auth', function(request, response) {
 
 app.get('/test', function(request, response) {
 	if (request.session.loggedin) {
-		response.sendFile('/test.html', { root: __dirname });
+		response.render('test.ejs',{user:JSON.stringify(request.session.username)});
+		//,{user:request.session.username}
+		//response.sendFile(path.join(__dirname,'/public/test.html'));
 	} else {
 		response.send('Please login to view this page!');
 	}
